95feed8b3fc2c98f459f1ed75e7777f78969db87
raza <raza2468@gmail.com>
2020-12-18 20:15:39 +0500

    d

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 // const { listeners } = require('process');
 var server = express()
 
-
+var PORT = process.env.PORT || 3001
 server.get('/', (req,res,next)=>{
 console .log("ha bhi chal gya hai");
 res.send("Samad bhi traet pakki hai")
@@ -15,7 +15,7 @@ res.send("tret done ho gai hai")
 
 
 
-server.listen(3111,()=>{
+server.listen(PORT,()=>{
     console.log("server man jao bhaee!");
 })
 
