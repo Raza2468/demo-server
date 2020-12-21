f0ac69b56536451db32221dfb90e4fde8af3e704
raza <raza2468@gmail.com>
2020-12-21 14:02:21 +0500

    e

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
-var express = require('express');
-// const { listeners } = require('process');
-var server = express()
-
-var PORT = process.env.PORT || 3001
-server.get('/', (req,res,next)=>{
-console .log("ha bhi chal gya hai");
-res.send("Samad bhi traet pakki hai")
-})
-
-server.get('/about',(req,res,next)=>{
-res.send("tret done ho gai hai")
-})
+   var express = require('express');
+   var server = express()
 
 
 
+    // server.get('/', (req,res,next)=>{
+    // res.send("Welcome to the <h1> Home </h1>")
+    // })
+
+   var PORT = process.env.PORT || 3001
+   server.get('/', (req,res,next)=>{
+   res.send("Welcome to the <h1> Home </h1>")
+ })
+
+   server.get('/about',(req,res,next)=>{
+   res.send("tret done ho gai hai <h1>About</h1>")
+   })
+
+//    server.listen(3001,()=>{
+//    console.log("server man jao bhaee!");
+//    })
 
 server.listen(PORT,()=>{
-    console.log("server man jao bhaee!");
+console.log("server man jao bhaee!");
 })
 
 
